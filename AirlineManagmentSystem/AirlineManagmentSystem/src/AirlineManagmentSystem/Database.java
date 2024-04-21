ea1aec94dfaa102777707f4d52830261ba308bca
Igor Kostoski <igorkostoski@ymail.com>
2024-04-21 18:14:13 +0200

    commit

--- AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Database.java
+++ AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Database.java
@@ -2,7 +2,10 @@ package AirlineManagmentSystem;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Database {
 	
@@ -11,14 +14,40 @@ public class Database {
 	private  String user = "user";
 	private  String pass = "123456";
 	
-	private  Connection connection;
+	private  Statement statement;
 	
 	
 	public Database() throws SQLException {
 		
-		connection = DriverManager.getConnection(url, user, pass);
+		Connection connection = DriverManager.getConnection(url, user, pass);
+		
+		statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+								ResultSet.CONCUR_READ_ONLY);
 		
 
+		
+	}
+	
+	public ArrayList<Passenger> getAllPassengers() throws SQLException{
+		String get = "SELECT * FROM `Passengers`;";
+		ResultSet rs = statement.executeQuery(get);
+		
+		ArrayList<Passenger> passengers = new ArrayList<>();
+		
+		while (rs.next()) {
+			Passenger p = new Passenger();
+			p.setId(Integer.parseInt(rs.getString("id")));
+			p.setFirstName(rs.getString("firstName"));
+			p.setLastName(rs.getString("lastName"));
+			p.setPhone(rs.getString("Phone"));
+			p.setEmail(rs.getString("email"));
+			
+			passengers.add(p);
+		}
+		
+		return passengers;
+		
+		
 		
 	}
 
--- AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/PassengersController.java
+++ AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/PassengersController.java
@@ -1,13 +1,32 @@
 package AirlineManagmentSystem;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PassengersController {
 	
-	public static void AddNewPassenger(Database database, Scanner s) {
+	public static void AddNewPassenger(Database database, Scanner s) throws SQLException {
 		
 		
-		System.out.println("");
+		System.out.println("Enter first name: ");
+		String firstName = s.next();
+		System.out.println("Enter last name: ");
+		String lastName = s.next();
+		System.out.println("Enter phone: ");
+		String Phone = s.next();
+		System.out.println("Enter email: ");
+		String email = s.next();
+		
+		Passenger passenger = new Passenger();
+		passenger.setFirstName(firstName);
+		passenger.setLastName(lastName);
+		passenger.setPhone(Phone);
+		passenger.setEmail(email);
+		ArrayList<Passenger> passengers = database.getAllPassengers();
+		
+		passenger.setId(passengers.get(passengers.size()-1).getId()+1);
+		
 		
 	}
 	
