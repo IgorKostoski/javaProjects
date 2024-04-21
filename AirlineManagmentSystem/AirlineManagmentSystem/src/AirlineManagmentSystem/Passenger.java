c11482efad325e7bbdba899fa868dab51b186633
Igor Kostoski <igorkostoski@ymail.com>
2024-04-21 16:49:29 +0200

    commit

--- AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Passenger.java
+++ AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Passenger.java
@@ -1,5 +1,54 @@
 package AirlineManagmentSystem;
 
 public class Passenger {
+	
+	private int id;
+	
+	private String firstName;
+	private String lastName;
+	private String Phone;
+	private String email;
+	
+	public Passenger() {
+		
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setPhone(String Phone) {
+		this.Phone = Phone;
+	}
+	
+	public String getPhone() {
+		return Phone;
+	}
+	
+	
+	
+	
+	
 
 }
