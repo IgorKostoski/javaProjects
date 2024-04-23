2ac23667295739462162d70951763b5d4f432aa8
Igor Kostoski <igorkostoski@ymail.com>
2024-04-23 21:55:06 +0200

    commit

--- AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Main.java
+++ AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Main.java
@@ -13,28 +13,6 @@ public class Main {
 		Database database = new Database();
 		Scanner s = new Scanner(System.in);
 		
-		int i = 0;
-		
-		do {
-			System.out.println("Welcome to Airline Manager System");
-			System.out.println("1. Add new passenger");
-			System.out.println("2. Get passenger id by name");
-			System.out.println("3. Edit passanger");
-			System.out.println("4. Quit");
-			
-			i = s.nextInt();
-			switch (i) {
-			case 1: 
-				PassengersController.AddNewPassenger(database, s);
-				break;
-			case 2:
-				PassengersController.findPassengerIdByName(database, s);
-				break;
-			case 3:
-				PassengersController.EditPassenger(database, s);
-				break;
-			}
-		} while (i!=4);
 		
 	
 
--- AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Passenger.java
+++ AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Passenger.java
@@ -54,13 +54,6 @@ public class Passenger {
 		return email;
 	}
 	
-	public void print() {
-		System.out.println("id: " + getId());
-		System.out.println("Name" + getFirstName()+ " " + getLastName());
-		System.out.println("Phone: " + getPhone());
-		System.out.println("Email :" + getEmail());
-		System.out.println();
-	}
 	
 	
 	
