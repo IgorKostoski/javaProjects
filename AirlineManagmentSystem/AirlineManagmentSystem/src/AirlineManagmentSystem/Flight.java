ac5cd50aa46ad0063facfb42ab2bb7e8fed85eff
Igor Kostoski <igorkostoski@ymail.com>
2024-04-29 20:09:19 +0200

    commit

---
+++ AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Flight.java
@@ -0,0 +1,108 @@
+package AirlineManagmentSystem;
+
+import java.time.LocalDateTime;
+
+public class Flight {
+	
+	private Airplane airplane;
+	private Airport origin;
+	private Airport destination;
+	private LocalDateTime departureTime;
+	private LocalDateTime arrivalTime;
+	private boolean delayed;
+	private int bookedEconomy;
+	private int bookedBusiness;
+	private Employee[] stuff;
+	private Passenger[] passengers;
+	
+	
+	public Flight() {
+		
+	}
+	
+	public Airplane getAirplane() {
+		return airplane;
+		
+	}
+	
+	public void setAirplane(Airplane airplane) {
+		this.airplane = airplane;
+		
+	}
+	
+	public Airport getOriginaAirport() {
+		return origin;
+	}
+	
+	public void setOriginAirport(Airport origin) {
+		this.origin = origin;
+	}
+	
+	public Airport getDestinationAirport() {
+		return destination;
+	}
+	
+	public void setDestinationAirport(Airport destination) {
+		this.destination = destination;
+	}
+	
+	
+	public LocalDateTime getDepartureTime() {
+		return departureTime;
+	}
+	
+	public void setDepartureTime(LocalDateTime departureTime) {
+		this.departureTime = departureTime;
+	}
+	
+	public LocalDateTime getArrivalTime() {
+		return arrivalTime;
+	}
+	
+	public void setArrivalTime(LocalDateTime arrivalTime) {
+		
+		this.arrivalTime = arrivalTime;
+	}
+	
+	public boolean isDelayed() {
+		return delayed;
+	}
+	
+	public void delay() {
+		delayed = true;
+	}
+	
+	public int getBookedEconomy() {
+		return bookedEconomy;
+	}
+	
+	public void setBookedEconomy(int bookedEconomy) {
+		this.bookedEconomy = bookedEconomy;
+	}
+	
+	public int getBookedBusiness() {
+		return bookedBusiness;
+	}
+	
+	public void  setBookedBusiness(int bookedBusiness) {
+		this.bookedBusiness = bookedBusiness;
+	}
+	
+	public Employee[] getStuff() {
+		return stuff;
+	}
+	
+	public void setStuff(Employee[] stuff) {
+		this.stuff = stuff;
+	}
+	
+	public Passenger [] getPassengers() {
+		return passengers;
+	}
+	
+	public void setPassengers(Passenger[] passengers) {
+		this.passengers = passengers;
+	}
+	
+
+}
