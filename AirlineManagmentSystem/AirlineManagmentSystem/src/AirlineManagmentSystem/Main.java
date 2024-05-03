b3fe5510dd0fc69cb8d5b16ef3669890cce90d4f
Igor Kostoski <igorkostoski@ymail.com>
2024-05-03 23:12:41 +0200

    commit

--- AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Main.java
+++ AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/Main.java
@@ -16,111 +16,11 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 
 
-		int i = 0;
-
-		do {
-			System.out.println("\nWelcome to Airline Managment System");
-			System.out.println("01. Add new passenger");
-			System.out.println("02. Get passenger  by name");
-			System.out.println("03. Print all passengers");
-			System.out.println("04. Edit passengers");
-			System.out.println("05. Delete passenger");
-			System.out.println("06. Add new employee");
-			System.out.println("07. Get employee by name");
-			System.out.println("08. Print all employees");
-			System.out.println("09. Edit employee");
-			System.out.println("10. Fire employee ");
-			System.out.println("11. Add new plane");
-			System.out.println("12. Print all planes");
-			System.out.println("13. Edited a plane");
-			System.out.println("14. Delete a plane");
-			System.out.println("15. Add new Airport");
-			System.out.println("16. Print all airports");
-			System.out.println("17. Edit an airport");
-			System.out.println("18. Delete an airport");
-			System.out.println("19. Create new flight");
-			System.out.println("20. SHow all flights");
-			System.out.println("21. Delay flight");
-			System.out.println("22. Book flight");
-			System.out.println("23. Set flight stuff");
-			System.out.println("24. Delete flight");
-			System.out.println("25. Quit");
-
-
-			i = s.nextInt();
-			switch (i) {
-			case 1:
-				PassengersController.AddNewPassenger(database, s);
-				break;
-			case 2:
-				PassengersController.findPassengerByName(database, s);
-				break;
-			case 3:
-				PassengersController.printAllPassengers(database);
-				break;
-			case 4:
-				PassengersController.EditPassenger(database, s);
-				break;
-			case 5:
-				PassengersController.DeletePassenger(database, s);
-				break;
-			case 6:
-				EmployeesController.AddNewEmployee(database, s);
-				break;
-			case 7:
-				EmployeesController.findEmployeeByName(database, s);
-				break;
-			case 8:
-				EmployeesController.printAllEmployees(database);
-				break;
-			case 9:
-				EmployeesController.editEmployee(database, s);
-				break;
-			case 10:
-				EmployeesController.deleteEmployee(database, s);
-				break;
-			case 11:
-				AirplanesController.AddNewAirplane(database, s);
-			case 12:
-				AirplanesController.PrintAllPlanes(database);
-				break;
-			case 13:
-				AirplanesController.EditAirplane(database, s);
-				break;
-			case 14:
-				AirplanesController.DeletePlane(database, s);
-				break;
-			case 15:
-				AirportsController.AddNewAirport(database, s);
-				break;
-			case 16:
-				AirportsController.PrintAllAirports(database);
-				break;
-			case 17:
-				AirportsController.EditAirport(database, s);
-				break;
-			case 18:
-				AirportsController.DeleteAirport(database, s);
-				break;
-			case 19:
-				FlightController.AddNewFlight(database, s);
-				break;
-			case 20:
-				FlightController.showAllFlights(database);
-				break;
-			
-
-			}
-		}
-		while( i != 25);
-
-
-
-
+		
 
 
 	}
-
+	
 
 
 }
