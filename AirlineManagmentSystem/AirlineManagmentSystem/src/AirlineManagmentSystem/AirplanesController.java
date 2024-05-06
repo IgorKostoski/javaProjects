945730e7383f449d56f137fe7b7747c6758b6829
Igor Kostoski <igorkostoski@ymail.com>
2024-05-06 21:58:47 +0200

    commit

--- AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/AirplanesController.java
+++ AirlineManagmentSystem/AirlineManagmentSystem/src/AirlineManagmentSystem/AirplanesController.java
@@ -7,10 +7,9 @@ import java.util.Scanner;
 
 public class AirplanesController {
 	
-	public static void AddNewAirplane(Database database, Scanner s) {
+	public static void AddNewAirplane(Database database, Scanner s) throws SQLException {
 		
-		System.out.println("Enter plane id(int): ");
-		int id = s.nextInt();
+		
 		System.out.println("Enter economy capacity (int): ");
 		int EconomyCapacity = s.nextInt();
 		System.out.println("Enter business capacity(int): ");
@@ -25,13 +24,30 @@ public class AirplanesController {
 		airplane.setBusinessCapacity(BusinessCapacity);
 		airplane.setModel(model);
 		
+		int id;
+		ArrayList<Airplane> planes = getAllPlanes(database);
+		
+		if (planes.size() != 0) {
+			
+			 id = planes.get(planes.size()-1).getId()+1;
+		} else {
+			id = 0;
+		}
+		airplane.setId(id);
+		
+		String insert = "INSERT INTO `airplanes`(`id`, `EconomyCapacity`, `BusinessCapacity`, `model`) "
+				+ "VALUES ('"+airplane.getId()+"',"
+						+ "'"+airplane.getEconomyCapacity()+"',"
+								+ "'"+airplane.getBusinessCapacity()+"',"
+										+ "'"+airplane.getModel()+"');";
+		
 	}
 	
 	
 	public static ArrayList<Airplane> getAllPlanes(Database database) throws SQLException {
 
 		ArrayList<Airplane> planes = new ArrayList<>();
-		String get = "SELECT * FROM `employees`;";
+		String get = "SELECT * FROM `airplanes`;";
 		
 		ResultSet rs = database.getStatement().executeQuery(get);
 		
@@ -41,6 +57,9 @@ public class AirplanesController {
 			a.setEconomyCapacity(rs.getInt("EconomyCapacity"));
 			a.setBusinessCapacity(rs.getInt("BusinessCapacity"));
 			a.setModel(rs.getString("model"));
+			planes.add(a);
+			
+			
 		}
 		
 		return planes;
