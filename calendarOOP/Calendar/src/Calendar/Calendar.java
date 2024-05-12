f231cb9eb657b52ead309a1770bb457492e0b07a
Igor Kostoski <igorkostoski@ymail.com>
2024-05-12 16:33:13 +0200

    commit

--- calendarOOP/Calendar/src/Calendar/Calendar.java
+++ calendarOOP/Calendar/src/Calendar/Calendar.java
@@ -2,11 +2,14 @@ package Calendar;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+
 
 public class Calendar extends JPanel {
 	
@@ -30,7 +33,18 @@ public class Calendar extends JPanel {
 		
 		top.add(date, BorderLayout.CENTER);
 		
+		JLabel left = new JLabel(new ImageIcon("pics/right-arrow.png"));
+		left.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		top.add(left,BorderLayout.EAST);
+		
+		JLabel right = new JLabel(new ImageIcon("pics/left-arrow.png"));
+		right.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		top.add(right,BorderLayout.WEST);
+		
+		
 		add(top, BorderLayout.NORTH);
+		
+		JPanel days = new JPanel(new GridLayout(7, 7));
 		
 	}
 
