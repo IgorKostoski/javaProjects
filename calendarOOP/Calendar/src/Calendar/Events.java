a0d77420ea2f2fbf83354291496e61c1f38b0fe0
Igor Kostoski <igorkostoski@ymail.com>
2024-05-14 22:15:58 +0200

    commit

--- calendarOOP/Calendar/src/Calendar/Events.java
+++ calendarOOP/Calendar/src/Calendar/Events.java
@@ -1,7 +1,10 @@
 package Calendar;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class Events extends JPanel{
@@ -10,7 +13,20 @@ public class Events extends JPanel{
 
 	public Events() {
 		
+		
+		setLayout(new BorderLayout(20, 20));
 		setBackground(Color.white);
+		setBorder(BorderFactory.createEmptyBorder(40,20,30,20));
+		
+		JPanel list = new JPanel(new GridLayout(4,1,10,10));
+		list.setBackground(Color.white); 
+		
+		
+		JPanel event = new  JPanel(new GridLayout(2,1));
+		event.setBorder(BorderFactory.createCompoundBorder(getBorder(), getBorder()));
+		
+		list.add(event);
+		
 		
 		
 		
