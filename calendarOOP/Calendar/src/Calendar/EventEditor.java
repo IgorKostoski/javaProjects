9fe584efb1511d76956442e3ed48a5586b0a454c
Igor Kostoski <igorkostoski@ymail.com>
2024-05-15 20:30:11 +0200

    commit

--- calendarOOP/Calendar/src/Calendar/EventEditor.java
+++ calendarOOP/Calendar/src/Calendar/EventEditor.java
@@ -2,11 +2,15 @@ package Calendar;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class EventEditor {
 	public EventEditor() {
@@ -23,6 +27,50 @@ public class EventEditor {
 		JPanel center = new JPanel(new GridLayout(3,2,20,20));
 		center.setBackground(Color.white);
 		
+		JLabel l1 = new JLabel("Title: ");
+		l1.setFont(new Font("Helvetica", Font.PLAIN, 20));
+		l1.setHorizontalAlignment(JLabel.CENTER);
+		center.add(l1);
+		
+		JTextField title = new JTextField();
+		title.setFont(new Font("Helvetica", Font.PLAIN, 20));
+		title.setHorizontalAlignment(JLabel.CENTER);
+		center.add(title);
+		
+		
+		
+		
+		JLabel l2 = new JLabel("Time: ");
+		l2.setFont(new Font("Helvetica", Font.PLAIN, 20));
+		l2.setHorizontalAlignment(JLabel.CENTER);
+		center.add(l2);
+		
+		JTextField time = new JTextField();
+		time.setFont(new Font("Helvetica", Font.PLAIN, 20));
+		time.setHorizontalAlignment(JLabel.CENTER);
+		center.add(time);
+		
+		JLabel l3 = new JLabel("Description: ");
+		l3.setFont(new Font("Helvetica", Font.PLAIN, 20));
+		l3.setHorizontalAlignment(JLabel.CENTER);
+		center.add(l3);
+		
+		JTextField description = new JTextField();
+		description.setFont(new Font("Helvetica", Font.PLAIN, 20));
+		description.setHorizontalAlignment(JLabel.CENTER);
+		center.add(description);
+		
+		
+		mainPanel.add(center, BorderLayout.CENTER);
+		
+		JPanel bottom = new JPanel(new GridLayout(1,2,20,20));
+		bottom.setBackground(null);
+		
+		JButton delete = new JButton("New");
+		delete.setFont(new Font("Helvetica", Font.PLAIN,20));
+		delete.setBackground(Color.CYAN);
+		delete.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		bottom.add(delete);
 		
 		
 		
