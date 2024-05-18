f5a987dd365c5bb3c0be59c63232596db0cdf446
Igor Kostoski <igorkostoski@ymail.com>
2024-05-18 22:10:54 +0200

    commit

---
+++ CurrencyConventer/src/CurrencyConverter.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class CurrencyConverter {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> currencyCodes = new HashMap<Integer, String>();
+
+
+    }
+}
