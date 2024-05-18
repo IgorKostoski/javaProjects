524b5819a25ae6039bf6bd275d466afcc2754ea4
Igor Kostoski <igorkostoski@ymail.com>
2024-05-18 21:24:58 +0200

    commit

---
+++ currencyConventer/src/main/java/com/currencyConventer.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.Scanner;
+
+public class currencyConventer {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type currency to convert from: ");
+
+        String convertFrom = scanner.nextLine();
+        System.out.println("Type currecny to convert to: ");
+        String convertTo = scanner.nextLine();
+        
+
+
+    }
+}
