d16f1de94cced9c9f07654aa68d12d11c1922a39
Igor Kostoski <igorkostoski@ymail.com>
2024-05-18 22:19:29 +0200

    commit

--- CurrencyConventer/src/CurrencyConverter.java
+++ CurrencyConventer/src/CurrencyConverter.java
@@ -23,6 +23,23 @@ public class CurrencyConverter {
 
         System.out.println("Welcome to the currency conventer!");
 
+        System.out.println("Currencty converting FROM?");
+
+        System.out.println("1: USD (Dollar)  \t 2:CAD (Canadian Dollar) " +
+                "\t 3:EUR (Euro) \t 4:HKD (Hong Kong Dollar) \t 5:INR (Indian Rupee)");
+
+        fromCode = currencyCodes.get(scanner.nextInt());
+
+        System.out.println("Currencty converting TO?");
+
+        System.out.println("1: USD (Dollar)  \t 2:CAD (Canadian Dollar) " +
+                "\t 3:EUR (Euro) \t 4:HKD (Hong Kong Dollar) \t 5:INR (Indian Rupee)");
+
+        toCode = currencyCodes.get(scanner.nextInt());
+
+
+
+
 
     }
 }
