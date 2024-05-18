f4a94025bb4ad318dbcb030a4e2acb833fd40ad6
Igor Kostoski <igorkostoski@ymail.com>
2024-05-18 21:58:46 +0200

    commit

--- currencyConventer/src/main/java/com/currencyConventer.java
+++ currencyConventer/src/main/java/com/currencyConventer.java
@@ -1,9 +1,18 @@
-package com;
+package  com;
 
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Scanner;
 
+
 public class currencyConventer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Type currency to convert from: ");
@@ -11,7 +20,31 @@ public class currencyConventer {
         String convertFrom = scanner.nextLine();
         System.out.println("Type currecny to convert to: ");
         String convertTo = scanner.nextLine();
-        
+        System.out.println("Type quantity to convert");
+        BigDecimal quantity = scanner.nextBigDecimal();
+
+        String urlString = "https://api.exchangeratesapi.io/v1/latest\n" +
+                "    ? access_key = 225650f2c8869c0a8f13a4027da15ee4" + convertFrom.toUpperCase();
+
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(urlString)
+                .get()
+                .build();
+
+        Response response  = client.newCall(request).execute();
+        String stringResponse = response.body().string();
+
+        JSONObject jsonObject = new JSONObject(stringResponse);
+        JSONObject ratesObject = jsonObject.getJSONObject("rates");
+        BigDecimal rate = ratesObject.getBigDecimal(convertTo.toUpperCase());
+
+        BigDecimal result = rate.multiply(quantity);
+        System.out.println(result);
+
+
+
 
 
     }
