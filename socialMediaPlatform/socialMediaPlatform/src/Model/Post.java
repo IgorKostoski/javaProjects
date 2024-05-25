3776b0246ced17092638368261cc02bd230166f1
Igor Kostoski <igorkostoski@ymail.com>
2024-05-25 23:06:08 +0200

    commit

--- socialMediaPlatform/socialMediaPlatform/src/Model/Post.java
+++ socialMediaPlatform/socialMediaPlatform/src/Model/Post.java
@@ -1,5 +1,59 @@
 package Model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Post {
+	
+	
+	private int ID;
+	private String content;
+	private User author;
+	private User user;
+	private LocalDateTime dateTime;
+	private ArrayList<Comment> comments;
+	private ArrayList<User> likes;
+	
+	
+	public Post() {
+		
+	}
+	
+	
+	public int getID() {
+		return ID;
+	}
+	
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	
+	
+	
+	
 
 }
