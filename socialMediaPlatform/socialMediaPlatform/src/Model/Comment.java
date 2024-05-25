93015e2a780afb2d350dffe611f6669179f485bb
Igor Kostoski <igorkostoski@ymail.com>
2024-05-25 23:11:49 +0200

    commit

--- socialMediaPlatform/socialMediaPlatform/src/Model/Comment.java
+++ socialMediaPlatform/socialMediaPlatform/src/Model/Comment.java
@@ -16,5 +16,42 @@ public class Comment {
 	}
 	
 	
+	public int getID() {
+		return ID;
+	}
+	
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void getContent(String content) {
+		this.content = content;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	public LocalDateTime getDateTime() {
+		return dateTime;
+		
+	}
+	
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	
+	
+	
+	
 
 }
