fbc608acd8110895d061b5271c4113b0f85bb4aa
Igor Kostoski <igorkostoski@ymail.com>
2024-06-06 00:20:36 +0200

    commit

--- socialMediaPlatform/socialMediaPlatform/src/View/ChangePassword.java
+++ socialMediaPlatform/socialMediaPlatform/src/View/ChangePassword.java
@@ -1,5 +1,37 @@
 package View;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+import Model.Database;
+import Model.User;
+
 public class ChangePassword {
+	
+	public ChangePassword(User user, Database database) {
+		
+		JFrame frame = new JFrame();
+		
+		JPanel panel = new JPanel(new BorderLayout());
+		
+		panel.setBackground(null);
+		panel.setBorder(BorderFactory.createEmptyBorder(83,99,175,99));
+		
+		panel.add(new JLabel ("Change Password", 40, GUIConstants.black, Font.BOLD)
+				,BorderLayout.NORTH);
+		
+		
+		JPanel center = new JPanel(new GridLayout(4,1,10,10));
+		center.setBackground(null);
+		center.setBorder(BorderFactory.createEmptyBorder(58,216,0,216));
+		
+		JTextField oldPassword = new JTextField("Old Password");
+		
+		
+	}
 
 }
