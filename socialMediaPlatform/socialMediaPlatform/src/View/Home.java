abc0a6b01839b33541ad2d6840745963e7b8ec94
Igor Kostoski <igorkostoski@ymail.com>
2024-06-08 22:06:31 +0200

    commit

--- socialMediaPlatform/socialMediaPlatform/src/View/Home.java
+++ socialMediaPlatform/socialMediaPlatform/src/View/Home.java
@@ -1,6 +1,9 @@
 package View;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
 
 import Model.Database;
 import Model.User;
@@ -11,6 +14,10 @@ public class Home {
 	public Home(User user, Database database) {
 		JFrame frame = new JFrame();
 		frame.getContentPane().setLayout(new BorderLayout());
+
+		JPanel sideBar = new JPanel();
+		sideBar.setBackground(GUIConstants.white);
+		Dimension sideBarDime = new Dimension(182,1000);
 		
 		
 		frame.setVisible(true);
