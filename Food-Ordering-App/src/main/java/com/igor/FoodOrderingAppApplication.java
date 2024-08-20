6af02ac68fb94ca59d501b498bc8b330932a89db
Igor Kostoski <igorkostoski@ymail.com>
2024-08-20 17:10:01 +0200

    commit

---
+++ Food-Ordering-App/src/main/java/com/igor/FoodOrderingAppApplication.java
@@ -0,0 +1,19 @@
+package com.igor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingAppApplication.class, args);
+		System.out.println("Hello World");
+
+
+	}
+
+
+
+
+}
