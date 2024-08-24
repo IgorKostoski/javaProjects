b6e0315403b5f80efecd1f95324c8f4a9f8af574
Igor Kostoski <igorkostoski@ymail.com>
2024-08-24 21:19:45 +0200

    commit

--- JavaGame/src/main/GamePanel.java
+++ JavaGame/src/main/GamePanel.java
@@ -1,26 +1,17 @@
 package main;
 
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
-	
-	
-	public GamePanel() {
-		
-		
-		
-	}
-	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		g.fillRect(100, 100, 200, 50);
-		
-		
-	}
-	
-	
+public class GamePanel extends JPanel {
 
+    public GamePanel() {
+        // Initialize game components here if needed
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);  // Clear the panel and call the superclass's method
+        g.fillRect(100, 100, 200, 50);  // Draw the rectangle
+    }
 }
--- JavaGame/src/main/GameWindow.java
+++ JavaGame/src/main/GameWindow.java
@@ -2,25 +2,14 @@ package main;
 
 import javax.swing.JFrame;
 
-public class GameWindow  {
-	private JFrame jframe;
+public class GameWindow {
+    private JFrame jframe;
 
-	
-	
-	public GameWindow(GamePanel gamePanel) {
-		
-		jframe = new JFrame();
-		
-		jframe.setSize(400, 400);
-		jframe.setVisible(true);
-		jframe.add(gamePanel);
-		
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-		
-		
-		
-	}
-
+    public GameWindow(GamePanel gamePanel) {
+        jframe = new JFrame();
+        jframe.setSize(400, 400);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);  // Add the GamePanel to the JFrame
+        jframe.setVisible(true);  // Make sure the JFrame is visible
+    }
 }
