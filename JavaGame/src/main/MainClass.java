3c2e9e69cc4f03e1fa1827da7589455862b516b4
Igor Kostoski <igorkostoski@ymail.com>
2024-08-24 20:12:55 +0200

    commit

--- JavaGame/src/main/MainClass.java
+++ JavaGame/src/main/MainClass.java
@@ -1,10 +1,9 @@
-package javaGameDemo;
+package main;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I am Alive");
 
 	}
 
