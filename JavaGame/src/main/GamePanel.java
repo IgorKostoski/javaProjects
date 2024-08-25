d80f9137753610b094fe886e950ad1abe84a9c28
Igor Kostoski <igorkostoski@ymail.com>
2024-08-25 17:32:28 +0200

    commit

--- JavaGame/src/main/GamePanel.java
+++ JavaGame/src/main/GamePanel.java
@@ -13,6 +13,8 @@ public class GamePanel extends JPanel {
 	
 	private MouseInputs mouseInputs;
 	private int xDelta = 100, yDelta = 100;
+	private int frames = 0;
+	private long lastCheck = 0;
 
     public GamePanel() {
         // Initialize game components here if needed
@@ -26,23 +28,30 @@ public class GamePanel extends JPanel {
     
     public void changeXDelta(int value) {
     	this.xDelta  += value;
-    	repaint();
+    	
     }
     
     public void changeYDelta(int value) {
     	this.yDelta += value;
-    	repaint();
+    	
     }
     
     public void setRectPos(int x, int y) {
     	this.xDelta = x;
     	this.yDelta = y;
-    	repaint();
+    	
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);  // Clear the panel and call the superclass's method
         g.fillRect(xDelta,yDelta, 200, 50);  // Draw the rectangle
+        frames++;
+        if(System.currentTimeMillis()- lastCheck >= 1000) {
+        	lastCheck = System.currentTimeMillis();
+        	System.out.println("FPS: " + frames);
+        	frames = 0;
+        }
+        repaint();
     }
 }
