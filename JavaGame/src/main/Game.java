6d7ce6c41718fbefaf168507b49f05bd0c2a19de
Igor Kostoski <igorkostoski@ymail.com>
2024-08-26 20:13:39 +0200

    commit

--- JavaGame/src/main/Game.java
+++ JavaGame/src/main/Game.java
@@ -31,14 +31,30 @@ public class Game implements Runnable{
     	long previousTime = System.nanoTime();
     	
     	int frames = 0;
-    	int update = 0;
+    	int updates = 0;
     	long lastCheck = System.currentTimeMillis();
+    	
+    	double deltaU = 0;
     	
     	
     	
     	while(true) {
     		
     		now = System.nanoTime();
+    		long currentTime = System.nanoTime();
+    		
+    		deltaU += (currentTime - previousTime) / timePerUpdate;
+    		
+    		previousTime = currentTime;
+    		
+    		if(deltaU >= 1) {
+    			updates++;
+    			deltaU--;
+    		}
+    		
+    		
+    		
+    		
     		if(now - lastFrame >= timePerFrame) {
     			
     			
@@ -52,8 +68,10 @@ public class Game implements Runnable{
     		 
     	        if(System.currentTimeMillis()- lastCheck >= 1000) {
     	        	lastCheck = System.currentTimeMillis();
-    	        	System.out.println("FPS: " + frames);
+    	        	System.out.println("FPS: " + frames + "| UPS : " 
+    	        			+updates);
     	        	frames = 0;
+    	        	updates = 0;
     	        }
     		
     	}
