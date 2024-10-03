741076842ef0c7b4333db28072f3a98af42432b6
Igor Kostoski <igorkostoski@ymail.com>
2024-10-03 19:17:27 +0200

    commit

--- JavaGame/src/entities/Entity.java
+++ JavaGame/src/entities/Entity.java
@@ -6,40 +6,36 @@ import java.awt.Rectangle;
 
 public abstract class Entity {
 
-	protected float x, y;
-	protected int width, height;
-	
-	protected Rectangle hitbox;
+    protected float x, y;
+    protected int width, height;
+    
+    protected Rectangle hitbox;
 
-	public Entity(float x, float y, int width, int height) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-	
-		initHitbox();
-		
-		
-	}
-	
-	protected void drawHitbox(Graphics g) {
-		//for debugging the hitbox
-		
-		g.setColor(Color.pink);
-		g.drawRect(hitbox.x, hitbox.y, hitbox.width, hitbox.height);
-		
-	}
-	private void initHitbox() {
-		hitbox = new Rectangle((int)x,(int)y,width,height);
-	}
-	
-	protected void updateHitbox() {
-		hitbox.x = (int)x;
-		hitbox.y = (int)y;
-	}
-	
-	public Rectangle getHitbox() {
-		return hitbox;
-	}
-
+    public Entity(float x, float y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        
+        initHitbox(x, y, width, height); // Initialize hitbox
+    }
+    
+    protected void drawHitbox(Graphics g) {
+        // For debugging the hitbox
+        g.setColor(Color.PINK);
+        g.drawRect(hitbox.x, hitbox.y, hitbox.width, hitbox.height);
+    }
+    
+    protected void initHitbox(float x, float y, float width, float height) {
+        hitbox = new Rectangle((int)x, (int)y, (int)width, (int)height);
+    }
+    
+    protected void updateHitbox() {
+        hitbox.x = (int)x;
+        hitbox.y = (int)y;
+    }
+    
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
 }
--- JavaGame/src/utilz/HelpMethods.java
+++ JavaGame/src/utilz/HelpMethods.java
@@ -4,7 +4,7 @@ import main.Game;
 
 public class HelpMethods {
 	
-	public static boolean CanMoveHere(float x, float y, int width, int height, int[][] lvlData) {
+	public static boolean CanMoveHere(float x, float y, float width, float height, int[][] lvlData) {
 		
 		if(!IsSolid(x,y, lvlData))
 			if(!IsSolid(x+width,y+height,lvlData))
@@ -18,17 +18,17 @@ public class HelpMethods {
 	
 	private static boolean IsSolid(float x, float y, int[][] lvlData) {
 		
-		if(x<0 || x> Game.GAME_WIDTH)
+		if(x<0 || x >= Game.GAME_WIDTH)
 			return true;
-		if(y<0 || y > Game.GAME_HEIGHT)
+		if(y<0 || y >= Game.GAME_HEIGHT)
 			return true;
 		
 		float xIndex = x / Game.TILES_SIZE;
 		float yIndex = y / Game.TILES_SIZE;
 		
-		int value  = lvlData[(int)xIndex][(int) yIndex];
+		int value  = lvlData[(int) yIndex][(int) xIndex];
 		
-		if(value >= 48 || value <0 || value == 11) 
+		if(value >= 48 || value <0 || value != 11) 
 			return true;
 		return false;
 		
