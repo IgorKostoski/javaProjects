cfc7173672d43d2057fe0d6cfb44771b2dcf9d1a
Igor Kostoski <igorkostoski@ymail.com>
2024-10-08 21:44:02 +0200

    commit

--- calendarOO/src/Calendar/Events.java
+++ calendarOO/src/Calendar/Events.java
@@ -1,7 +1,13 @@
 package Calendar;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Events extends JPanel{
@@ -10,7 +16,51 @@ public class Events extends JPanel{
 
 	public Events() {
 		
-		setBackground(Color.LIGHT_GRAY);
+		
+		setLayout(new BorderLayout(20,20));
+		setBackground(Color.white);
+		
+		setBorder(BorderFactory.createEmptyBorder(40, 20,30, 20));
+		
+		
+		JPanel list = new JPanel(new GridLayout(4,1,10,10));
+		list.setBackground(Color.white);
+		
+		
+		for (int i=0; i<7;i++) {
+
+			
+			JPanel event = new JPanel(new GridLayout(2,1));
+			event.setBorder(BorderFactory.createCompoundBorder(
+					BorderFactory.createEmptyBorder(10, 10, 10, 10),
+					BorderFactory.createMatteBorder(0,10,0,0, Color.decode("#00d1e8"))));
+			
+			event.setBackground(Color.decode("#f0f0f0"));
+			event.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			
+			
+			JLabel title = new JLabel("Event Title");
+			title.setBorder(BorderFactory.createEmptyBorder(0,15,0,15));
+			title.setFont(new Font("Helvetica", Font.PLAIN, 18));
+			title.setForeground(Color.black);
+			
+			event.add(title);
+			
+			JLabel time = new JLabel("Event Time");
+			time.setBorder(BorderFactory.createEmptyBorder(5,15,4,15));
+			time.setFont(new Font("Helvetica", Font.PLAIN, 14));
+			time.setForeground(Color.DARK_GRAY);
+			event.add(time);		
+			
+			
+			
+			
+			
+			list.add(event);
+		}
+		
+		add(list, BorderLayout.CENTER);
+		
 		
 	}
 
