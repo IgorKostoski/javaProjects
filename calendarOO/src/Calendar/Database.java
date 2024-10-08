1b93edb9e3b77645ae17f7ffce200afebbb1a1e2
Igor Kostoski <igorkostoski@ymail.com>
2024-10-08 22:37:30 +0200

    commit

--- calendarOO/src/Calendar/Database.java
+++ calendarOO/src/Calendar/Database.java
@@ -1,9 +1,7 @@
 package Calendar;
 
 import java.sql.Statement;
-
-
-
+import java.util.ArrayList;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -29,6 +27,64 @@ public class Database {
 		}
 	}
 	
+	
+	public ArrayList<Event> getEvents(String date) {
+		
+		ArrayList<Event> events = new ArrayList<>();
+		
+		String select = "SELECT * FROM `Calendar` WHERE `Date` = '';";
+		
+		try {
+			ResultSet rs = statement.executeQuery(select);
+			while (rs.next()) {
+				Event e = new Event();
+				e.setID(rs.getInt("ID"));
+				e.setTitle(rs.getString("Title"));
+				e.setDescription(rs.getString("Description"));
+				e.setDateTimeFromString(rs.getString("Date")+" | "+ rs.getString("Time"));
+				events.add(e);
+				
+			}
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+			
+		}
+		return events;
+		
+	}
+	
+	public boolean hasEvent(String date) {
+		boolean hasEvent = false;
+		String select = "SELECT * FROM `Calendar` WHERE `Date` = '';";
+		try {
+			ResultSet rs = statement.executeQuery(select);
+			
+			hasEvent = rs.next();
+			
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+			
+		}
+		return hasEvent;
+	}
+	
+	
+	public void createEvent(Event e) {
+		String insert = "INSERT INTO `Calendar`(`Title`, `Description`, `Date`, `Time`) "
+				+ "VALUES ('"+e.getTitle()+"','"+e.getDescription()+"',"
+						+ "'"+e.getDateToString()+"','"+e.getTimeToString()+"')";
+		
+		try {
+			statement.execute(insert);
+		} catch (SQLException exception) {
+			
+			exception.printStackTrace();
+		}
+	}
+	
+	public void updateEvent(Event e) {
+		
+	}
 	
 	
 	
