9d13f7ad42b6501d51ed6c49483c84098d8498c7
Igor Kostoski <igorkostoski@ymail.com>
2024-10-27 21:35:45 +0100

    commit

--- calendarOO/src/Calendar/Event.java
+++ calendarOO/src/Calendar/Event.java
@@ -24,7 +24,7 @@ public class Event {
 	
 	
 	
-	public Event(LocalDate date) {
+	public Event(LocalDate date) {// Title ==> null(new Event)
 		
 		dateTime = LocalDateTime.of(date, LocalTime.now());
 		
