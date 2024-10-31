a50d32b750351490cbef04bc9df66d00f41e0888
Igor Kostoski <igorkostoski@ymail.com>
2024-10-31 22:04:03 +0100

    commit

--- JavaCalendaInOOP/src/Calendar/Database.java
+++ JavaCalendaInOOP/src/Calendar/Database.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 
 public class Database {
@@ -29,6 +30,79 @@ public class Database {
 		}
 	}
 	
+	
+	public ArrayList<Event> getEvents(String date){
+		
+		ArrayList<Event> events = new ArrayList<>();
+		String select = "SELECT * FROM `Calendar` WHERE `Date` =  '';";
+		
+		try {
+			ResultSet rs = statement.executeQuery(select);
+			while (rs.next()) {
+				Event e = new Event();
+				e.setID(rs.getInt("ID"));
+				e.setDescription(rs.getString("Title"));
+				e.setDescription(rs.getString("Description"));
+				e.setDateTImeFromString(rs.getString("Date")+ " |  " +rs.getString("Time"));
+				events.add(e);
+				
+				
+			}
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+		return events;
+	}
+	
+	
+	public boolean hasEvent(String date) {
+		boolean hasEvent = false;
+		String select = "SELECT * FROM `Calendar` WHERE `Date` =  '';";
+		
+		try {
+			ResultSet rs = statement.executeQuery(select);
+			hasEvent = rs.next();
+		
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+		
+		
+		
+		return hasEvent;
+	}
+	
+	
+	public void createEvent(Event e) {
+	    String insert = "INSERT INTO `Calendar`( `Title`, `Description`, `Date`, `Time`)"
+	            + " VALUES ('" + e.getTItle() + "','" + e.getDescription() + "','" + e.getDateToString() + "','" + e.getTimeToString()+ "');";
+	    try {
+	        statement.execute(insert);
+	    } catch (SQLException exception) {
+	        exception.printStackTrace();
+	    }
+	}
+	
+	public void updateEvent(Event e) {
+		String update = "UPDATE `Calendar` SET `Title`='"+e.getTItle()+"',`Description`='"+e.getDescription()+"',"
+				+ "`Date`='"+e.getDateToString()+"',`Time`='"+e.getTimeToString()+"' WHERE `ID` = "+e.getID()+" ;";
+		
+		try {
+	        statement.execute(update);
+	    } catch (SQLException exception) {
+	        exception.printStackTrace();
+	    }
+	}
+	
+	public void deleteEvent(int ID) {
+		String delete = "DELETE FROM `Calendar` WHERE `ID` = "+ID+" ;";
+		
+		try {
+	        statement.execute(delete);
+	    } catch (SQLException exception) {
+	        exception.printStackTrace();
+	    }
+	}
 	
 
 }
